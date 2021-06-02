53fa2e95341a5921806014c6f88cf3dc6c5eaee0
Scott Ford <swford@ualberta.ca>
2021-06-02 10:16:23 -0600

    refactored code and created function to get tweepy query parameters

--- tweets_db.py
+++ tweets_db.py
@@ -1,46 +1,41 @@
-import tweepy
+from datetime import datetime
 import sqlalchemy
-import os
+import tweepy
+from dateutil.parser import parse
+from config_db import db_connect
 from config_twitter import create_twitter_api
 from config_tone import create_ibm_api, tone_data
-from datetime import datetime
-from dateutil.parser import parse
 
-# create DB connection
-MYSQL_PASS = os.environ['MYSQL_PASS']
-engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{MYSQL_PASS}@localhost/TwitterDB')
-connection = engine.connect()
-metadata = sqlalchemy.MetaData()
+def query_params():
+    # specify search arguments for tweepy tweet query
+    while True:
+        try:
+            the_date = datetime.now()
+            specific_date = f'{the_date.year}-{the_date.month}-{the_date.day}'
+            query = input('Enter your query word: ')
+            num_tweets = int(input('Enter the number of tweets you would like returned: '))
+            break
+        except TypeError:
+            print('''
+            Looks like you entered an invalid datatype. Try again.
+            ''')
+    return specific_date, query, num_tweets
 
-# create table objects
+# call imported functions: DB, twitter API, tone analyzer client
+engine,connection, metadata = db_connect()
+api = create_twitter_api()
+tone_analyzer, end_point = create_ibm_api()
+
+# create/initialize table objects
 user_table = sqlalchemy.Table('users', metadata, autoload=True, autoload_with=engine)
 tweet_table = sqlalchemy.Table('tweets', metadata, autoload=True, autoload_with=engine)
 
-# create twitter API object
-api = create_twitter_api()
-# create ibm API client object and endpoint object
-tone_analyzer, end_point = create_ibm_api()
-
-# specify query word and tweet amount
-while True:
-    try:
-        the_date = datetime.now()
-        specific_date = f'{the_date.year}-{the_date.month}-{the_date.day}'
-        search_word = input('Enter your query word: ')
-        num_tweets = int(input('Enter the number of tweets you would like returned: '))
-        break
-    except TypeError:
-        print('''
-        Looks like you entered an invalid datatype. Try again
-        ''')
-
 # get tweets
-tweets = tweepy.Cursor(api.search, q=search_word, lang='en', since=specific_date, tweet_mode='extended').items(num_tweets)
-tweet_list = []
+specific_date, query, num_tweets = query_params()
+tweets = tweepy.Cursor(api.search, q=query, lang='en', since=specific_date, tweet_mode='extended', result_type='recent').items(num_tweets)
 
 # loop through tweets data and add tweets to list
-for tweet in tweets:
-    tweet_list.append(tweet._json)
+tweet_list = [tweet._json for tweet in tweets]
 
 for tweet in tweet_list:
     # specify insert values for retweets and normal tweets
